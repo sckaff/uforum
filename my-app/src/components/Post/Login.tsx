6752bb1173dd20619de68835abbefdb0a7c68310
Christian Scaff <cscaff@ufl.edu>
2023-02-16 14:46:51 -0500

    Minor Refactoring. Ignore

--- my-app/src/components/Post/Login.tsx
+++ my-app/src/components/Post/Login.tsx
@@ -1,42 +1,30 @@
-import { TextField, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import React, { useState } from "react";
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import InputLabel from '@mui/material/InputLabel';
 import Input from '@mui/material/Input';
 
 
-export default function InputAdornments() {
-    const [showPassword, setShowPassword] = React.useState(false);
-  
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault();
-    };
-}
-
-    export const Login = () => {
+export const Login = () => {
+      
     type myStates = {
-        email: string,
-        passWord: string,
+          email: string,
+          passWord: string,
     }
-    const [email, setEmail] = useState('');
-    const [passWord, setPassWord] = useState('');
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(email);
+      event.preventDefault();
+      console.log(email);
     }
+    
+    const [email, setEmail] = useState('');
+    const [passWord, setPassWord] = useState('');
     const [showPassword] = React.useState(false);
-  
+
     return (
         <div className="loginPage">
             <div className="card">
                 <form onSubmit={handleSubmit}>
-                <InputLabel htmlFor="UFL_email">Enter UFL Email</InputLabel>
+                <InputLabel htmlFor="UFL_email">UFL Email</InputLabel>
                 <Input
                     id="UFL_email"
                     value={email}
