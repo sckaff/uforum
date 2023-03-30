4b7e3d0e258677a44ead998c343559bf024cae5f
Christian Scaff <cscaff@ufl.edu>
2023-03-29 20:14:00 -0400

    Installed and created a unit test via Jest

---
+++ front-end/src/setupTests.test.tsx
@@ -0,0 +1,25 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+import { render, fireEvent, screen, within } from "@testing-library/react";
+import Homebar from "./components/other/Homebar";
+import React, { useState } from "react";
+import { createMemoryHistory } from 'history';
+import { BrowserRouter } from 'react-router-dom';
+
+//HomeBar Navigation Tests
+
+test('Testing Home Bar Button Text Presence', () => {
+  const history = createMemoryHistory();
+  render(
+    <BrowserRouter>
+      <Homebar loggedIn={false}/>
+    </BrowserRouter>
+  );
+  expect(screen.getByTestId('LogInBtn')).toHaveTextContent('Login');
+  expect(screen.getByTestId('HomeBtn')).toHaveTextContent('Home');
+  expect(screen.getByTestId('SearchBtn')).toHaveTextContent('Search');
+});
+
