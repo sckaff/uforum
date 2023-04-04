3bc470f4198af0f17dff9ae9c337f2e99cb6bcde
Christian Scaff <cscaff@ufl.edu>
2023-04-04 15:07:21 -0400

    Started Working on Post UI. Still need to figure out how to place main post fixed in the top rather than the middle of the page. Need to work on comment section being srollable. Also discovered that at least for me I am getting unhandled errors when trying to register an existing account and for some reason I cannot log in to an accoun that already exists. I'll have to look more into this later.

--- front-end/src/components/Post/CommentSection.tsx
+++ front-end/src/components/Post/CommentSection.tsx
@@ -13,7 +13,8 @@ export default function CommentSectionNew(props: {postID: number}) {
     const [comments, setComments] = useState<Array<Comment>>([]);
     const [body, setBody] = useState<string>("");
     const [loggedIn, setLoggedIn] = useState<boolean>(false);
-
+    const [commentInputBox, setCommentInputBox] = useState<string>("text-sm text-gray-base w-full mr-3 py-5 px-11 h-2 border rounded mb-2 overflow-hidden");
+    const [errorMsg, setErrorMsg] = useState<string>("flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-10 invisible");
     useEffect(() => {
         fetch('http://localhost:8080/getcomments/' + props.postID)
         .then((res) => res.json())
@@ -28,7 +29,8 @@ export default function CommentSectionNew(props: {postID: number}) {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-    
+        setCommentInputBox("text-sm text-gray-base w-full mr-3 py-5 px-11 h-2 border rounded mb-2 overflow-hidden");
+        setErrorMsg("flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-10 invisible");
         if (loggedIn) {
             const token = authService.getToken();
             const comment: comment_input = {
@@ -55,12 +57,14 @@ export default function CommentSectionNew(props: {postID: number}) {
         }
         else {
             console.log("NOT LOGGED IN CANNOT CREATE POST")
+            setCommentInputBox("text-sm text-gray-base w-full mr-3 py-5 px-11 h-2 border rounded mb-2 overflow-hidden border-red-500");
+            setErrorMsg("flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-10 visible");
         }
     }
 
     const commentList = comments.map((comment) => {
         return (
-            <div key={comment.id} className="max-w-sm rounded overflow-hidden shadow-lg border-2 p-2">
+            <div key={comment.id} className="min-w-96 rounded overflow-hidden shadow-lg border-2 p-2">
                 <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{comment.user}</div>
                 <div data-cy={"comment-" + comment.body}>{comment.body}</div>
             </div>
@@ -71,8 +75,10 @@ export default function CommentSectionNew(props: {postID: number}) {
         <div>
             <div>
                 <form onSubmit={handleSubmit}>
-                    <input data-cy="comment-input" id="body" value={body} type="text" placeholder="Comment" onChange={(e) => setBody(e.target.value)} required/><br/>
-                    <button data-cy="comment-submit" type="submit">Submit</button>
+                    <br/>
+                    <input className={commentInputBox} data-cy="comment-input" id="body" value={body} type="text" placeholder="Comment" onChange={(e) => setBody(e.target.value)} required/><br/>
+                    <div className={errorMsg}>You must be logged in to comment!</div>
+                    <button className="bg-orange-400 w-full mt-4 rounded mb-2 overflow-hidden shadow-lg" data-cy="comment-submit" type="submit">Submit</button>
                 </form>
             </div>
             <div>
--- front-end/src/components/Post/PostView.tsx
+++ front-end/src/components/Post/PostView.tsx
@@ -28,19 +28,25 @@ export default function PostView() {
     }
     else {
         return (
-            <div>
-                <div>
-                    <p>{post.user}: {post.title}</p>
-                </div>
-                <div>
-                    <p>{post.body}</p>
-                </div>
-                <div>
-                    <p data-cy="comment-title">Comments:</p>
-                        <div>
-                          <CommentSection postID={post.id}/> 
+            <div className="h-screen flex flex-col items-center justify-center  ">
+                <div className=" min-w-96 w-1/2 rounded overflow-hidden shadow-lg border-gray-200 ">
+                    <div className="min-w-96 rounded overflow-hidden shadow-lg border-gray-200">
+                        <div className="text-xl font-sans font-bold">
+                            {post.title}
                         </div>
-
+                        <div className="text-lg font-sans italic inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                            Posted By: {post.user}
+                        </div>
+                        <div className="text-lg font-sans">
+                            {post.body}
+                        </div>
+                    </div>
+                    <div className="min-w-96 rounded overflow-hidden shadow-lg border-gray-200">
+                    <p data-cy="comment-title" className="text-lg font-sans font-bold">Comments:</p>
+                        <div>
+                        <CommentSection postID={post.id}/> 
+                        </div>
+                    </div>
                 </div>
             </div>
         );
