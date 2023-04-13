766af5b1ddc23d240d28925b9585b9074342da78
Christian Scaff <cscaff@ufl.edu>
2023-04-13 13:37:41 -0400

    Cleaned up Post View + Added upvote counter

--- front-end/src/components/Post/PostView.tsx
+++ front-end/src/components/Post/PostView.tsx
@@ -29,24 +29,37 @@ export default function PostView() {
     else {
         return (
             <div className="flex flex-col items-center justify-center mt-1  ">
-                <div className=" min-w-96 w-2/3 rounded overflow-hidden shadow-lg border-2 border-sky-500">
-                    <div className="min-w-96 border-2 rounded overflow-hidden shadow-lg mb-1 border-gray-200">
-                        <div className="text-xl font-sans font-bold">
-                            {post.title}
+                <div className="relative min-w-96 w-2/3 rounded overflow-hidden shadow-lg border-2 border-sky-500">
+                        <div className="m-3">
+                            <div className="text-xl font-sans font-bold">
+                                {post.title}
+                            </div>
+                            <div className="text-lg font-sans italic inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                Posted By: {post.user}
+                            </div>
+                            <div className="text-lg font-sans italic inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                {post.category}
+                            </div>
                         </div>
-                        <div className="text-lg font-sans italic inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                            Posted By: {post.user}
+                        <div className="absolute top-0 right-0 m-3">
+                                +1
+                                <button>⬆</button>
+                                <button>⬇</button>
                         </div>
-                        <div className="text-lg font-sans">
+                        <div className="text-lg font-sans ml-3">
                             {post.body}
                         </div>
-                    </div>
-                    <div className="min-w-96 rounded overflow-hidden shadow-lg border-gray-200">
-                        <div>
-                        <CommentSection postID={post.id}/> 
+                        <br/>
                         </div>
-                    </div>
+                        <br/>
+                        <div className="relative min-w-96 w-2/3 rounded overflow-hidden shadow-lg border-2 border-sky-500">
+                            <div className="min-w-96 rounded overflow-hidden shadow-lg border-gray-200">
+                                <div>
+                                <CommentSection postID={post.id}/> 
+                                </div>
+                            </div>
                 </div>
+                <br/><br/>
             </div>
         );
     }
